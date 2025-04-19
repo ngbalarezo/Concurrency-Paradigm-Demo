546fa4af8d797787fb4ce8a0c1dcc4b6567b49b8
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-19 13:43:28 -0400

    Fleshed out main intro and thread objects.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
 import CustomThreads.EnemyCheckThread;
 import CustomThreads.EnemySpawnThread;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("main is starting...");
+        System.out.println();
+        System.out.println("Press enter to continue...");
+
+        EnemySpawnThread enemy = new EnemySpawnThread("Werewolf John");
+
+        Thread enemyCheck = new Thread(new EnemyCheckThread(), "Enemy Radar");
 
 
     }
