9f1f52be18be7ef1c8a5b9ddd9336859905b79f6
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-19 13:39:14 -0400

    Created custom threads and structured thir files.

---
+++ src/CustomThreads/EnemyCheckThread.java
@@ -0,0 +1,9 @@
+package CustomThreads;
+
+// corresponds to method 1 of "Creating Threads in Java" notes
+public class EnemyCheckThread implements Runnable {
+    @Override
+    public void run() {
+        // insert thread task
+    }
+}
---
+++ src/CustomThreads/EnemySpawnThread.java
@@ -0,0 +1,14 @@
+package CustomThreads;
+
+// corresponds to method 2 of "Creating Threads in Java" notes
+public class EnemySpawnThread extends Thread {
+    // constructor
+    public EnemySpawnThread(String threadName) {
+        super(threadName);
+    }
+
+    @Override
+    public void run() {
+        // insert thread task
+    }
+}
