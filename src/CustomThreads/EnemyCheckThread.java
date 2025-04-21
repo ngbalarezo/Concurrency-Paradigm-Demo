e30a6fa4c4f43676d27f665c930f47ea1d082755
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-21 19:27:03 -0400

    Rewrote Main and fleshed out demo introduction.

--- src/CustomThreads/EnemyCheckThread.java
+++
@@ -1,9 +0,0 @@
-package CustomThreads;
-
-// corresponds to method 1 of "Creating Threads in Java" notes
-public class EnemyCheckThread implements Runnable {
-    @Override
-    public void run() {
-        // insert thread task
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,48 @@
 import java.util.Scanner;
-import CustomThreads.EnemyCheckThread;
-import CustomThreads.EnemySpawnThread;
-
 
 public class Main {
+    public static boolean enemyNearby = false;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // begin program
         System.out.println("main is starting...");
-        System.out.println();
         System.out.println("Press enter to continue...");
+        scanner.nextLine();
 
-        EnemySpawnThread enemy = new EnemySpawnThread("Werewolf John");
+        // begin intro demo
+        System.out.println("Here is a demonstration of the Java Thread class:");
+        System.out.println("Press enter to start demo threads...");
+        scanner.nextLine();
 
-        Thread enemyCheck = new Thread(new EnemyCheckThread(), "Enemy Radar");
+        // create introductory threads and start them
+        Thread EnemySpawnThread = new Thread(()-> {
+            try {
+                Thread.sleep(10000);
+                enemyNearby = true;
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt();
+                return;
+            }
+        });
 
+        Thread EnemyCheckThread = new Thread(()->{
+            while(enemyNearby == false){
+                System.out.println("No enemies nearby.");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread was interrupted: " + e.getMessage());
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+            System.out.println("1 Enemy Nearby.");
+        });
+
+        EnemySpawnThread.start();
+        EnemyCheckThread.start();
 
     }
 }
