b1acc66b5c5f601c491856180be6cb2383df0172
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-22 21:33:19 -0400

    Added custom classes.

---
+++ src/CustomClasses/Demo.java
@@ -0,0 +1,24 @@
+package CustomClasses;
+import java.util.Random;
+import java.time.Instant;
+import java.time.Duration;
+
+public class Demo {
+    public String demoName;
+    public int arraySum;
+
+    // constructor
+    public Demo(String demoName) {
+        this.demoName = demoName;
+        this.arraySum = 0;
+    }
+
+//    public static void singleThreadedSum {
+//
+//    }
+//
+//    public static void multiThreadedSum {
+//
+//    }
+
+}
